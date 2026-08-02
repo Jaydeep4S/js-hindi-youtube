6771a1aa91a615e43c7fc70cb548e3d2ae54ac0b
Jaydeep4S <142020948+Jaydeep4S@users.noreply.github.com>
2026-08-02 14:19:22 +0000

    Memory - Stack & Heap

--- 01_basics/05_datatype-summery.js
+++ 01_basics/05_datatype-summery.js
@@ -21,7 +21,7 @@ let userEmail;
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-console.log(id===anotherId);  // here both are different
+// console.log(id===anotherId);  // here both are different
 
 // const bigNumber = 527352752873468468836286n   // bigInt
 
@@ -36,4 +36,31 @@ const myFunction = function(){
     console.log("Hello World")
 }
 
-// 
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+/* ============== Memory ===============================
+   There are two types of memory
+   1> Stack ( use in primitive type) we get copy in which we can change value
+   2> Heap ( use in Non- Primitive) we get referance
+
+*/
+
+let myYubename = "mytestchennel"
+let anotherName = "woohooo channer"
+
+console.log(myYubename);
+console.log(anotherName);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "jaydeep@google.com"
+
+console.log(userOne);
+console.log(userTwo);
+
