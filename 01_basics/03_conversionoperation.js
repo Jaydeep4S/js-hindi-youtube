73ef9841cf7f39e3ae474d01fd2e0f69302ecfd4
Jaydeep4S <142020948+Jaydeep4S@users.noreply.github.com>
2026-08-02 11:11:12 +0000

    Operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -20,8 +20,39 @@ let booleanIsLoogedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// =================== Operations ===========================
+
+let value = 3
+let negValue =  -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Jaydeep"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 +"2");
+// console.log("1"+ 2+ 2);
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+// prefix and postfix js mdn
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
 
 
 
