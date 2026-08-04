882e673c35fe112959e2e4e22fee38c4a982f6b5
Jaydeep4S <142020948+Jaydeep4S@users.noreply.github.com>
2026-08-04 05:13:33 +0000

    string Understanding

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -4,3 +4,16 @@ const repoCount = 12
 //console.log(name+repoCount+" Value");
 
 console.log(`Hello, My name is ${name}, and my repository count is ${repoCount}`);
+
+const gameName = new String('jaydeephc')
+
+console.log(gameName[0]);
+console.log(gameName[2]);
+console.log(gameName.__proto__);  // prototype double underscore
+console.log(gameName.length); //length of string
+console.log(gameName.toUpperCase()); // for upper case
+console.log(gameName.charAt('2')); // at index which charrector
+console.log(gameName.indexOf('d')); //  charrector at which index
+
+
+
