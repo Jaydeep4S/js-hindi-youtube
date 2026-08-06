06a68708665588040394d29903a596c57b3945e5
Jaydeep4S <142020948+Jaydeep4S@users.noreply.github.com>
2026-08-06 06:43:59 +0000

    number

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,9 @@
+const   score = 400  // It automatically detect data type
+console.log(score);
+
+const balance = new Number(100) // we can define data type 
+console.log(balance);   // print as an object
+
+console.log(balance.toString());  // converts number into string
+console.log(balance.toString().length); //converts number into string and provide length of number
+
